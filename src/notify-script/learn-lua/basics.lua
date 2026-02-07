c43cfb1720e48be4cd6573cf8f510ac27cb94454
Stanislav Dolgachov <s.dolgachov@gmail.com>
2026-02-07 10:53:17 +0200

    chore: add comments to lua basics script

--- src/notify-script/learn-lua/basics.lua
+++ src/notify-script/learn-lua/basics.lua
@@ -10,20 +10,24 @@ local truth, lies = true, false
 local nothing = nil
 
 -- Functions
+-- Declaratio #1
 local someFunc = function(name)
     print('Hello, '..name..'!')
 end
 
 someFunc('Bob')
 
+-- Declaration #2
 local function some_stuff() 
     print("some stuff")
 end
 
 some_stuff()
 
+-- Closure
 local function counter()
     local count = 0 
+    
     return function()
         count = count + 1
         return count

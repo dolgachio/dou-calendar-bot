a0504c61d9f3aa9ce2598bc4c4cc9c77d581de1c
Stanislav Dolgachov <s.dolgachov@gmail.com>
2026-02-22 11:16:17 +0200

    feat: add data parsing from RSS feed

--- src/notify_script/src/main.lua
+++ src/notify_script/src/main.lua
@@ -1,9 +1,10 @@
-print("start fetching the RSS feed...")
+local http_request = require "http.request"
+local feedparser = require "feedparser"
 
 local calendar_rss_feed_link = "https://dou.ua/calendar/feed/"
 
-local http_request = require "http.request"
-local feedparser = require "feedparser"
+print("🚀 start fetching the RSS feed... from " .. calendar_rss_feed_link)
+
 local headers, stream = assert(http_request.new_from_uri(calendar_rss_feed_link):go())
 local body = assert(stream:get_body_as_string())
 
@@ -11,5 +12,29 @@ if headers:get ":status" ~= "200" then
     error(body)
 end
 
-print(body)
-print(feedparser)
+print("✅ Data fetched from: " .. calendar_rss_feed_link)
+print("🔄 Start parsing data")
+
+local parsed_data = feedparser.parse(body)
+
+if parsed_data == nil then
+    print("😔 No RSS data")
+    return
+end
+
+print "📝 Parsed data details:"
+print("------")
+print("Entries:")
+
+print(parsed_data.entries[1]);
+
+for index, value in ipairs(parsed_data.entries) do
+    print(index.."."..value.title.." updated "..value.updated)
+    print("\n")
+    print("🔗‍️ Link: \n"..value.link)
+    print("📝 Summary: \n"..value.summary)
+    print("📝 Summary Length: \n"..#value.summary)
+    print("\n")
+end
+
+-- print(parsed_data.toString())
